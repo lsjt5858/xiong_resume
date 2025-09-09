f23286eef68c037ee85a8374dc15c1e81ad240fc
lsjt5858 <15988107785@163.com>
2025-09-09 12:39:58 +0800

    edit config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 
-  import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
 
@@ -51,7 +51,7 @@
     },
     build: {
       target: 'esnext',
-      outDir: 'build',
+      outDir: 'dist',
     },
     server: {
       port: 3000,
